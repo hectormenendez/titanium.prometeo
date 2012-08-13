5cfa17fa7faeb3f12eeed86874f706d6c47e2d3f
Hector Menendez <etor.mx@gmail.com>
2012-08-13 06:09:41 -0500

    Module: Device. Simple device detection.

---
+++ device.js
@@ -0,0 +1,23 @@
+
+exports.isApple     = false;
+exports.isAndroid   = false;
+exports.isTablet    = false;
+exports.name        = Ti.Platform.osname;
+exports.version     = Ti.Platform.version;
+exports.height      = Ti.Platform.displayCaps.platformHeight;
+exports.width       = Ti.Platform.displayCaps.platformWidth;
+
+switch (exports.name) {
+    case 'ipad'   :
+    case 'iphone' :
+        exports.isApple  = true;
+        break;
+    case 'android':
+        exports.isAndroid = true;
+        break;
+    default:
+        exports.isSupported = false;
+}
+
+if (exports.name == 'ipad' || exports.height > 899 || exports.width > 899)
+    exports.isTablet = true;
