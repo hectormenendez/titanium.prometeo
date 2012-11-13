82df05a42c55f75d5a6f7964020949f85437f42e
Janet Rivas <jrivas@yappme.com>
2012-11-13 01:37:01 -0600

    add alertoptionalDialog to UI

--- ui.js
+++ ui.js
@@ -64,7 +64,8 @@ element = [
     { name: 'buttonfacebook' , method : Ti.Facebook.createLoginButton },//TODO test it
     { name: 'picker'         , method : Ti.UI.createPicker            },
     { name: 'buttonbar'      , method : Ti.UI.createButtonBar         },
-    { name: 'dialog'   		 , method : Ti.UI.createAlertDialog		  }
+    { name: 'dialog'   		 , method : Ti.UI.createAlertDialog		  },
+    { name: 'optionaldialog' , method : Ti.UI.createOptionDialog	  }
 ];
 
 // Apple specific element
