4b2bcd568a63f2605200be45ee2859ba7d5122ab
Héctor Menéndez <etor.mx@gmail.com>
2012-12-19 02:01:58 -0600

    Net behaves differently on Android, fixed it.

--- net.js
+++ net.js
@@ -11,42 +11,42 @@ exports.xhr = function(obj){
 
 	var config = Core.isDefined(Config.Net)? Config.Net : { timeout : 5000 };
 
-    if (typeof obj != 'object') return Core.error('sys:net:xhr:object');
-    if (typeof obj.success != 'function') obj.success = function(){};
-    if (typeof obj.load    != 'function') obj.load    = function(){};
-    if (typeof obj.error   != 'function') obj.error   = function(){};
-    if (typeof obj.header  != 'object')   obj.header  = {};
-    if (typeof obj.data    != 'string')   obj.data    = '';
-    if (typeof obj.url     != 'string')   obj.url     = '';
-    // type's string; Enforce uppercase 'n make sure we get correct transfer type
-    if ( typeof obj.type != 'string' ||  ((obj.type = obj.type.toUpperCase()) && (
-            obj.type != 'POST'   &&
-            obj.type != 'GET'    &&
-            obj.type != 'DELETE' &&
-            obj.type != 'PUT'
-        )))
-        return Core.error('sys:net:xhr:type');
+	if (typeof obj != 'object') return Core.error('sys:net:xhr:object');
+	if (typeof obj.success != 'function') obj.success = function(){};
+	if (typeof obj.load    != 'function') obj.load    = function(){};
+	if (typeof obj.error   != 'function') obj.error   = function(){};
+	if (typeof obj.header  != 'object')   obj.header  = {};
+	if (typeof obj.data    != 'string')   obj.data    = '';
+	if (typeof obj.url     != 'string')   obj.url     = '';
+	// type's string; Enforce uppercase 'n make sure we get correct transfer type
+	if ( typeof obj.type != 'string' ||  ((obj.type = obj.type.toUpperCase()) && (
+			obj.type != 'POST'   &&
+			obj.type != 'GET'    &&
+			obj.type != 'DELETE' &&
+			obj.type != 'PUT'
+		)))
+		return Core.error('sys:net:xhr:type');
 
-    // url's string; must only contain a-z
-    if (typeof obj.url != 'string' ) return Core.error('sys:net:xhr:url');
+	// url's string; must only contain a-z
+	if (typeof obj.url != 'string' ) return Core.error('sys:net:xhr:url');
 
-    var log = function(message, action){
-        message = Core.stringify(message).replace(/(\s|\\n)+/g, '');
-        message = message.replace(/\\\"+/g,'"');
+	var log = function(message, action){
+		message = Core.stringify(message).replace(/(\s|\\n)+/g, '');
+		message = message.replace(/\\\"+/g,'"');
 		Core.log(message, 'sys:net:xhr:{'+ action +'}');
-    };
+	};
 
-    // setup connection
-    var xhr  = Ti.Network.createHTTPClient({
-    	// This has to be set to something slighty higher than our timeout
-    	// otherwise the default timeout value will trigger.
-    	timeout                    : parseInt(config.timeout*1.2,10),
-        enableKeepAlive            : false,
-        validatesSecureCertificate : false
-    });
+	// setup connection
+	var xhr  = Ti.Network.createHTTPClient({
+		// This has to be set to something slighty higher than our timeout
+		// otherwise the default timeout value will trigger.
+		timeout                    : parseInt(config.timeout*1.2,10),
+		enableKeepAlive            : false,
+		validatesSecureCertificate : false
+	});
 
 	var interval;
-    var timer = new Date().getTime();
+	var timer = new Date().getTime();
 	var tmp;
 
 	var timeout = function(){
@@ -60,40 +60,48 @@ exports.xhr = function(obj){
 			tmp.status = 1; // our definition of timeout.
 			xhr.onerror.call(tmp);
 		}, 100);
+	};
+
+	xhr.onload = function(){
+		log(this, 'loaded');
+		obj.load.call(this, this.responseText);
+
+		// TODO : Apple triggers onreadystate 4 only once (as it should, android does not.)
+		if (Core.Device.isApple || this.readyState !== 4 || this.status !== 200)
+			return false;
+		log(this, 'success');
+		obj.success.call(this, this.responseText);
+	};
+
+	xhr.onerror = function(){
+		log(this, 'error');
+		obj.error.call(this, this.responseText);
+	};
+
+	xhr.onreadystatechange = function(e){
+		if (this.readyState === 1) timeout.call(this);
+		else tmp = this;
+		log(this, 'state ' + this.readyState);
+		if (interval && this.readyState === 4) clearInterval(interval);
+		// TODO : Apple triggers onreadystate 4 only once (as it should, android does not.)
+		if (Core.Device.isAndroid || this.readyState !== 4 || this.status !== 200)
+			return false;
+		log(this, 'success');
+		obj.success.call(this, this.responseText);
+	};
+
+	log(obj.url, obj.type);
+	xhr.open(obj.type, obj.url);
+
+	// setup headers
+	xhr.setRequestHeader('X-HTTP-Method-Override', obj.type);
+	if (Core.count(obj.header)) for (var name in obj.header){
+		log(name + ': ' + obj.header[name], 'header');
+		xhr.setRequestHeader(name, obj.header[name]);
 	}
 
-    xhr.onload = function(){
-        obj.load.call(this, this.responseText);
-        log(this, 'loaded');
-    };
+	log(obj.data, 'data');
+	xhr.send(obj.data);
 
-    xhr.onerror = function(){
-		obj.error.call(this, this.responseText);
-		log(this, 'error');
-    };
-
-    xhr.onreadystatechange = function(e){
-    	if (this.readyState === 1) timeout.call(this);
-    	else tmp = this;
-        log(this, 'state ' + this.readyState);
-		if (interval && this.readyState === 4) clearInterval(interval);
-        if (this.readyState !== 4 || this.status !== 200) return false;
-        log(this, 'success');
-        obj.success.call(this, this.responseText);
-    };
-
-    log(obj.url, obj.type);
-    xhr.open(obj.type, obj.url);
-
-    // setup headers
-    xhr.setRequestHeader('X-HTTP-Method-Override', obj.type);
-    if (Core.count(obj.header)) for (var name in obj.header){
-        log(name + ': ' + obj.header[name], 'header');
-        xhr.setRequestHeader(name, obj.header[name]);
-    }
-
-    log(obj.data, 'data');
-    xhr.send(obj.data);
-
-    return xhr;
+	return xhr;
 };
